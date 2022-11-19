1f0448277cb5320243a517f5bffe611aa7eaccca
jrodr117 <jrodr113@msudenver.edu>
2022-11-19 10:17:26 -0700

    added a cursor to be able to write sql commands

--- Business Layer/main.py
+++ Business Layer/main.py
@@ -3,8 +3,6 @@ class Task:
         self.description = description
         self.completed = completed
         self.ID = ID
-    
+
     def __str__(self):
         return f'{self.description} "(Done!)"'
-
- 
--- Data Layer/sqlitedb.py
+++ Data Layer/sqlitedb.py
@@ -1,3 +1,4 @@
 import sqlite3
 
 conn = sqlite3.connect("task_list_db.sqlite")
+cursor = conn.cursor()
