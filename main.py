f78011c1027f43ffaf631b4783b43b619742f784
jrodr117 <jrodr113@msudenver.edu>
2022-11-19 13:33:11 -0700

    creating my own db works best using db that the professor provided doesnt take my inputs at all even though I verified all types matched up

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 class Task:
-    def __init__(self, description, completed, ID=0):
+    def __init__(self, description, completed, taskid):
         self.description = description
         self.completed = completed
-        self.ID = ID
+        self.taskid = taskid
 
     def __str__(self):
         return f'{self.description} "(Done!)"'
--- sqlitedb.py
+++ sqlitedb.py
@@ -3,13 +3,20 @@ from main import *
 
 conn = sqlite3.connect("task_list_db.sqlite")
 cursor = conn.cursor()
+conn.execute(
+    "CREATE TABLE Task(description TEXT NOT NULL, completed INTEGER NOT NULL,taskID INTEGER PRIMARY KEY AUTOINCREMENT);"
+)
 
-task1 = Task("Walk Harlee", 0, None)
 
+# task1 = Task("Walk Harlee", 0, None)
+task1 = Task("Feed Harlee", 0, None)
 cursor.execute(
-    "INSERT INTO Task VALUES (:description, :completed, :ID)",
-    {"description": task1.description, "completed": task1.completed, "ID": task1.ID},
+    "INSERT INTO Task VALUES (?,?,?)",
+    (task1.description, task1.completed, task1.taskid),
+    # "INSERT INTO Task VALUES (:description, :completed, :ID)",
+    # {"description": task1.description, "completed": task1.completed, "ID": task1.ID},
 )
 
 conn.commit()
 conn.close()
+
