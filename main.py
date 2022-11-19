8ee2c6fadb6a47fa24f746ddecd7d15cafa0f68e
jrodr117 <jrodr113@msudenver.edu>
2022-11-19 14:13:22 -0700

    Finally figured out the order of my commit to the DB matters. So i changed the order so it matches that of the table.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 class Task:
-    def __init__(self, description, completed, taskid):
+    def __init__(self, description, completed, ID):
         self.description = description
         self.completed = completed
-        self.taskid = taskid
+        self.ID = ID
 
     def __str__(self):
         return f'{self.description} "(Done!)"'
--- sqlitedb.py
+++ sqlitedb.py
@@ -4,29 +4,17 @@ from main import *
 conn = sqlite3.connect("task_list_db.sqlite")
 cursor = conn.cursor()
 
-
-conn.execute(
-    "CREATE TABLE Task(description TEXT NOT NULL, completed INTEGER NOT NULL,taskID INTEGER PRIMARY KEY AUTOINCREMENT);"
-)
-
-conn.execute(
-    """INSERT INTO Task (description, completed) Values
-    ('Get Bike Fixed', 1),
-    ('Call your mom',1),
-    ('Buy toothbrush',0),
-    ('Do homework',0);"""
-)
-
 task1 = Task("Walk Harlee", 0, None)
-# task1 = Task("Feed Harlee", 0, None)
+
+# def create_task(task_object)
+#     withc conn:
+
 cursor.execute(
-    # "INSERT INTO Task VALUES (?,?,?)",
-    # (task1.description, task1.completed, task1.taskid),
-    "INSERT INTO Task VALUES (:description, :completed, :ID)",
+    "INSERT INTO Task VALUES (:ID, :description, :completed)",
     {
+        "ID": task1.ID,
         "description": task1.description,
         "completed": task1.completed,
-        "ID": task1.taskid,
     },
 )
 
