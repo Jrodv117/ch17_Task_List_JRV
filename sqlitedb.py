b983b2548f6478939383f7e4b8ca4c05af59f8fc
jrodr117 <jrodr113@msudenver.edu>
2022-11-19 14:16:43 -0700

    created a create function in the db file to create an  new row in the db

--- sqlitedb.py
+++ sqlitedb.py
@@ -6,17 +6,18 @@ cursor = conn.cursor()
 
 task1 = Task("Walk Harlee", 0, None)
 
-# def create_task(task_object)
-#     withc conn:
 
-cursor.execute(
-    "INSERT INTO Task VALUES (:ID, :description, :completed)",
-    {
-        "ID": task1.ID,
-        "description": task1.description,
-        "completed": task1.completed,
-    },
-)
+def create_task(task_object):
+    with conn:
+        cursor.execute(
+            "INSERT INTO Task VALUES (:ID, :description, :completed)",
+            {
+                "ID": task_object.ID,
+                "description": task_object.description,
+                "completed": task_object.completed,
+            },
+        )
+
 
 conn.commit()
 conn.close()
